db2a3ba9a795495d4227166453eb9dffb404f9ab
mmatin2007 <matin.mehmood2007@gmail.com>
2024-05-20 08:52:49 +0200

    sfondo

--- Codice/main.py
+++ Codice/main.py
@@ -1,18 +1,30 @@
 import pygame, sys
 from pygame.locals import *
+from sfondo import *
 
-WINDOW_SIZE = (600, 400)
+WINDOW_SIZE = (1920, 1080)
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 pygame.display.set_caption('Finestra Base')
 
 clock = pygame.time.Clock()
 fps = 60
 
+# varibili di gioco
+
+
+# immagini
+sfondo_img = pygame.image.load("Immagini/immagine_sfondo.jpg")
+sfondo_img = pygame.transform.scale(sfondo_img, (1920, 1080))
+sfondoRect = Sfondo(screen, (0,0), sfondo_img, 5)
+# sfondoRect.velocity = 5
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    sfondoRect.draw()
+    sfondoRect.move()
 
 
 
@@ -21,13 +33,4 @@ while True:
 
 
     pygame.display.flip()
-
-
-
-
-
     clock.tick(fps)
-
-
-
-
---
+++ Codice/sfondo.py
@@ -0,0 +1,25 @@
+import pygame
+from pygame import *
+
+class Sfondo:
+    def __init__(self, screen, pos, image, velocity = 5) -> None:
+        self.screen = screen
+        self.pos = pos
+        self.image = image
+        self.velocity = velocity
+
+        self.rect = []
+        self.rect.append(self.image.get_rect())
+        self.rect.append(self.image.get_rect())
+
+    def draw(self):
+        self.screen.blit(self.image, self.rect[0],self.rect[1])
+    
+    def move(self):
+        if self.rect[1].x == 0:
+            self.rect[0].x = 1921
+        elif self.rect[0].x == 0:
+            self.rect[1].x = 1921
+    
+        self.rect[0].x -= self.velocity
+        self.rect[1].x -= self.velocity
